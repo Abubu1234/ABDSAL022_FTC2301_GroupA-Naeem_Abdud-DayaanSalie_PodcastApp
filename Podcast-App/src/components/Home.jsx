497df37f54808ad6da19a167ffd3f5ef4e1854f6
Abdud-Dayaan <abduddayaansalie@gmail.com>
2023-08-03 05:20:17 +0200

    added main home page

--- Podcast-App/src/components/Home.jsx
+++ Podcast-App/src/components/Home.jsx
@@ -1,140 +1,199 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Dialogs from './Dialogs';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CircularProgress } from '@mui/material';
+import GroupSizesColors from './Favourite';
+import Carousel from './Carousel';
+import TextField from '@mui/material/TextField';
 
-
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
-export default function () {
-  return ( 
+const CardSetUp = () => {
+  const [cards, setCards] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+  const [sortingOption, setSortingOption] = React.useState('A-Z');
+
+  React.useEffect(() => {
+    fetch("https://podcast-api.netlify.app/")
+      .then((res) => res.json())
+      .then((data) => {
+        setCards(data);
+        setIsLoading(false);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const genreMap = {
+    1: "Personal Growth",
+    2: "True Crime and Investigative Journalism",
+    3: "History",
+    4: "Comedy",
+    5: "Entertainment",
+    6: "Business",
+    7: "Fiction",
+    8: "News",
+    9: "Kids and Family",
+  };
+
+  function formatGenres(genreIds) {
+    return genreIds.map((genreId) => genreMap[genreId]).join(", ");
+  }
+
+  function formatTime(timeString) {
+    const updateTime = new Date(timeString);
+    return updateTime.toLocaleString("en-US", {
+      dateStyle: "medium",
+    });
+  }
+
+  // Format the "updated" field for each show
+  cards.forEach((show) => {
+    show.updated = formatTime(show.updated);
+  });
+
+  // Sorting logic based on the selected sorting option
+  const sortedCards = React.useMemo(() => {
+    const sorted = [...cards];
+
+    if (sortingOption === "A-Z") {
+      sorted.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sortingOption === "Z-A") {
+      sorted.sort((a, b) => b.title.localeCompare(a.title));
+    } else if (sortingOption === "Ascending") {
+      sorted.sort((a, b) => new Date(a.updated) - new Date(b.updated));
+    } else if (sortingOption === "Descending") {
+      sorted.sort((a, b) => new Date(b.updated) - new Date(a.updated));
+    } else if (sortingOption === "Genre") {
+      sorted.sort((a, b) => formatGenres(a.genres).localeCompare(formatGenres(b.genres)));
+    }
+
+    return sorted;
+  }, [cards, sortingOption]);
+
+  const [searchQuery, setSearchQuery] = React.useState('');
+
+  const handleSearch = (event) => {
+    setSearchQuery(event.target.value);
+  };
+  // Filter the cards based on the search query
+  const filteredCards = React.useMemo(() => {
+    return sortedCards.filter((card) =>
+      card.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+  }, [sortedCards, searchQuery]);
+  
+
+
+ 
+
+  const carouselSlides = sortedCards.map((card) => ({
+    id: card.id,
+    title: card.title,
+    image: card.image,
+  }));
+
+  
+
+
+  return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-        <Box 
-                component="img"
-                sx={
-                    {
-                        height:"auto" ,
-                        width:{
-                            xs: 45, //0
-                            sm: 45, //600
-                            md: 45, //900
-                            lg: 45, //1200
-                            xl: 45, //1536
-                        }
-                    }
-                }
-                src="../android-chrome-192x192.png"  
-                alt="nav-img" 
-                />
-          <Typography variant="h6" color="inherit" noWrap>
-            Album layout
-          </Typography>
-        </Toolbar>
-      </AppBar>
+
       <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: 'background.paper',
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
-              Album layout
+        {isLoading ? (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh',
+            }}
+          >
+            <CircularProgress sx={{ height: '150vh' }} color="secondary" />
+            <Typography variant="h5" color="text.primary">
+              Loading...
             </Typography>
-            <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Something short and leading about the collection below—its contents,
-              the creator, etc. Make it short and sweet, but not too short so folks
-              don&apos;t simply skip over it entirely.
-            </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Main call to action</Button>
-              <Button variant="outlined">Secondary action</Button>
-            </Stack>
-          </Container>
-        </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="div"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random?wallpapers"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
-                </Card>
+          </Box>
+        ) : (
+          <>
+             <Grid item xs={12} sm={6} md={4} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                  <Carousel slides={carouselSlides} />
+                </Grid>
+                <Grid item xs={12} md={6}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', maxWidth: 400, margin: '0 auto' }}>
+
+      <TextField
+        label="Search"
+        variant="outlined"
+        value={searchQuery}
+        onChange={handleSearch}
+        sx={{ display: 'flex', justifyContent: 'center' }}
+      />
+       </Box>
+    </Grid>
+            <Container sx={{ py: 8 }} maxWidth="md">
+              
+            <GroupSizesColors onSortingOptionChange={setSortingOption} />
+              <Grid container spacing={4}>
+                 {/* Add the Carousel here */}
+                
+
+                {/* Render the rest of the cards */}
+                {filteredCards.map((card) => (
+                  <Grid item key={card.id} xs={12} sm={6} md={4}>
+                    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                      <CardMedia
+                        component="span"
+                        sx={{
+                          pt: '100%',
+                        }}
+                        image={card.image}
+                        onLoad={() => setIsLoading(false)}
+                      />
+                      <CardContent sx={{ flexGrow: 1 }}>
+                        <Typography gutterBottom variant="h5" component="h2">
+                          {card.title}
+                        </Typography>
+                        <Typography component="h2">Genres: {formatGenres(card.genres)}</Typography>
+                        <Typography component="h2">Seasons: {card.seasons}</Typography>
+                        <Typography component="h2">Updated: {card.updated}</Typography>
+                      </CardContent>
+                      <CardActions>
+                        <Dialogs
+                          cardid={card.id}
+                          cardimage={card.image}
+                          cardtitle={card.title}
+                          carddescription={card.description}
+                          cardseasons={card.seasons}
+                          cardgenres={formatGenres(card.genres)}
+                          cardupdated={card.updated}
+                        />
+                      </CardActions>
+                    </Card>
+                  </Grid>
+                ))}
               </Grid>
-            ))}
-          </Grid>
-        </Container>
+            </Container>
+          </>
+        )}
       </main>
-      {/* Footer */}
-      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Something here to give the footer a purpose!
-        </Typography>
-        
-      </Box>
-      {/* End footer */}
+      <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer"></Box>
+      <Typography variant="subtitle1" align="center" color="text.secondary" component="p">
+        Icon made by <a href=""> Reion </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </Typography>
     </ThemeProvider>
   );
-}
+};
+
+export default CardSetUp;
