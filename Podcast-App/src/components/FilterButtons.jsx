18b47d159be9c9ff2e9296918a6090ee7d061fef
Abdud-Dayaan <abduddayaansalie@gmail.com>
2023-08-03 05:20:09 +0200

    added filter buttons

---
+++ Podcast-App/src/components/FilterButtons.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
+import Grid from '@mui/material/Grid';
+
+const GroupSizesColors = ({ onSortingOptionChange }) => {
+  return (
+    <Grid container spacing={2} justifyContent="center" alignItems="center" sx={{ paddingBottom: '5px' }}>
+      <Grid item xs={12} sm={6} md={4}>
+        <ButtonGroup
+          fullWidth
+          size="large"
+          color="primary"
+          aria-label="sorting button group"
+        >
+          <Button style={{ color:'#A305BA'  }} onClick={() => onSortingOptionChange('A-Z')}>A-Z</Button>
+          <Button style={{ color:'#A305BA'  }} onClick={() => onSortingOptionChange('Z-A')}>Z-A</Button>
+        </ButtonGroup>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <ButtonGroup
+          fullWidth
+          size="large"
+          color="primary"
+          aria-label="sorting button group"
+        
+        >
+          <Button style={{ color:'#A305BA'  }} onClick={() => onSortingOptionChange('Ascending')}>Ascending</Button>
+          <Button style={{ color:'#A305BA'  }} onClick={() => onSortingOptionChange('Descending')}> Descending</Button>
+        </ButtonGroup>
+      </Grid>
+      <Grid item xs={12} sm={6} md={4}>
+        <ButtonGroup
+          fullWidth
+          size="large"
+          color="primary"
+          aria-label="sorting button group"
+        >
+          <Button style={{ color:'#A305BA'  }} onClick={() => onSortingOptionChange('Genre')}>Sort by Genre</Button>
+        </ButtonGroup>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default GroupSizesColors;
